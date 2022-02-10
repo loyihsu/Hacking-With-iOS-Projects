195b17e4e8f17324b4bc934424cc01c4fdb532a0
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-02-11 02:16:08 +0800

    Add open website and delegate method to set title.

--- Project 4.playground/Contents.swift
+++ Project 4.playground/Contents.swift
@@ -18,6 +18,41 @@ class ViewController : UIViewController, WKNavigationDelegate {
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Open",
+            style: .plain,
+            target: self,
+            action: #selector(openTapped)
+        )
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+
+    @objc func openTapped() {
+        let alertController = UIAlertController(
+            title: "Open page...",
+            message: nil,
+            preferredStyle: .actionSheet)
+        alertController.addAction(
+            UIAlertAction(title: "apple.com", style: .default, handler: openPage)
+        )
+        alertController.addAction(
+            UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage)
+        )
+        alertController.addAction(
+            UIAlertAction(title: "Cancel", style: .cancel)
+        )
+
+        alertController.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(alertController, animated: true)
+    }
+
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
     }
 }
 
