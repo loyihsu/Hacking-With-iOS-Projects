3e188f954210dbff922671eb48a33c53925308c4
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-02-11 02:22:12 +0800

    Add a toolbar and progress indicator.

--- Project 4.playground/Contents.swift
+++ Project 4.playground/Contents.swift
@@ -6,6 +6,7 @@ import PlaygroundSupport
 
 class ViewController : UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
+    var progressView: UIProgressView!
 
     override func loadView() {
         webView = WKWebView()
@@ -19,16 +20,39 @@ class ViewController : UIViewController, WKNavigationDelegate {
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
 
+        webView.addObserver(self,
+                            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+                            options: .new,
+                            context: nil)
+
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             title: "Open",
             style: .plain,
             target: self,
             action: #selector(openTapped)
         )
+
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh,
+                                      target: webView,
+                                      action: #selector(webView.reload))
+
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
     }
 
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
 
     @objc func openTapped() {
