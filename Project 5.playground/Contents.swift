9994d0b6740e1b39b110c59b81f2d1669b481971
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-02-11 10:14:54 +0800

    Setup for Project 5.

---
+++ Project 5.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class TableViewController : UITableViewController {
+    
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = UINavigationController(rootViewController: TableViewController())
