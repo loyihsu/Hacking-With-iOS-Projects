0bec34a89ddae52f2aef3af86a823d33c9f72edd
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-02-15 02:06:51 +0800

    Refine the solutions and add challenge solutions.

--- Project 7.playground/Contents.swift
+++ Project 7.playground/Contents.swift
@@ -24,7 +24,6 @@ class TableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
-
         titleLabel = UILabel()
         titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 14)
 
@@ -38,7 +37,13 @@ class TableViewCell: UITableViewCell {
         subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
 
         titleLabel.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
-        titleLabel.leftAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
+        titleLabel.leftAnchor.constraint(
+            equalTo: contentView.safeAreaLayoutGuide.leftAnchor, constant: 20
+        ).isActive = true
+        titleLabel.rightAnchor.constraint(
+            equalTo: contentView.safeAreaLayoutGuide.rightAnchor, constant: 20
+        ).isActive = true
+
 
         subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 0).isActive = true
         subtitleLabel.leftAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
@@ -48,8 +53,11 @@ class TableViewCell: UITableViewCell {
 
 class TableViewController : UITableViewController {
     var petitions: [Petition] = []
+    var displayedPetitions: [Petition] = []
 
     let myCustomCellId = "My Cell"
+
+    var filtered = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,9 +65,17 @@ class TableViewController : UITableViewController {
 
         let urlString: String
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Credit", style: .plain, target: self, action: #selector(creditTapped))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            title: "Filter", style: .plain, target: self, action: #selector(filterTapped)
+        )
+
         if navigationController?.tabBarItem.tag == 0 {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         } else {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
 
@@ -73,14 +89,14 @@ class TableViewController : UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return displayedPetitions.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: myCustomCellId, for: indexPath) as! TableViewCell
 
-        cell.titleLabel.text = petitions[indexPath.row].title
-        cell.subtitleLabel.text = petitions[indexPath.row].body
+        cell.titleLabel.text = displayedPetitions[indexPath.row].title
+        cell.subtitleLabel.text = displayedPetitions[indexPath.row].body
         return cell
     }
 
@@ -94,6 +110,7 @@ class TableViewController : UITableViewController {
         let decoder = JSONDecoder()
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            displayedPetitions = petitions
             tableView.reloadData()
         }
     }
@@ -106,6 +123,51 @@ class TableViewController : UITableViewController {
         )
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
+    }
+
+    @objc func creditTapped() {
+        let controller = UIAlertController(
+            title: "Credits",
+            message: "The data comes from the We The People API of the Whitehouse.",
+            preferredStyle: .alert)
+
+        controller.addAction(UIAlertAction(title: "Close", style: .cancel))
+
+        present(controller, animated: true)
+    }
+
+    @objc func filterTapped() {
+        let controller = UIAlertController(
+            title: "Filter", message: nil, preferredStyle: .alert
+        )
+
+        var textField: UITextField?
+
+        controller.addTextField {
+            textField = $0
+        }
+
+        controller.addAction(UIAlertAction(title: "Done", style: .default) { [self] _ in
+            if let field = textField, let text = field.text {
+                displayedPetitions = petitions.filter {
+                    $0.title.contains(text) || $0.body.contains(text)
+                }
+                navigationItem.leftBarButtonItem = UIBarButtonItem(
+                    title: "Show all", style: .plain, target: self, action: #selector(restoreUnfiltered)
+                )
+                tableView.reloadData()
+            }
+        })
+
+        present(controller, animated: true)
+    }
+
+    @objc func restoreUnfiltered() {
+        displayedPetitions = petitions
+        tableView.reloadData()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            title: "Filter", style: .plain, target: self, action: #selector(filterTapped)
+        )
     }
 }
 
@@ -144,11 +206,18 @@ class DetailViewController: UIViewController {
 
 let tabBarController = UITabBarController()
 
+tabBarController.view.frame = CGRect(x: 0, y: 0, width: 320, height: 568)
+
 let content = UINavigationController(
     rootViewController: TableViewController()
 )
 
-content.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
+let topRated = UINavigationController(
+    rootViewController: TableViewController()
+)
 
-tabBarController.viewControllers = [ content ]
+content.tabBarItem = UITabBarItem(tabBarSystemItem: .recents, tag: 0)
+topRated.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
+
+tabBarController.viewControllers = [ content, topRated ]
 PlaygroundPage.current.liveView = tabBarController
