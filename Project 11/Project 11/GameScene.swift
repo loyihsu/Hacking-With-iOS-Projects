fa9294d124ea5ef714526df9340733584982807f
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-02-23 22:13:39 +0800

    Add box implementations.

---
+++ Project 11/Project 11/GameScene.swift
@@ -0,0 +1,35 @@
+//
+//  GameScene.swift
+//  Project 11
+//
+//  Created by Yu-Sung Loyi Hsu on 2022/2/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+            box.position = location
+            addChild(box)
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
