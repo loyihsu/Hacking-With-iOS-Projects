819000c2f65e977a12cfb3c92748b753b5b76f1e
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-02-23 23:31:53 +0800

    Add editing mode and score handler.

--- Project 11/Project 11/GameScene.swift
+++ Project 11/Project 11/GameScene.swift
@@ -9,12 +9,40 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var scoreLabel: SKLabelNode!
+    var editLabel: SKLabelNode!
+
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+
+    var editingMode: Bool = false {
+        didSet {
+            editLabel.text = editingMode
+            ? "Done"
+            : "Edit"
+        }
+    }
+
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background.jpg")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
+
+        editLabel = SKLabelNode(fontNamed: "Chalkduster")
+        editLabel.text = "Edit"
+        editLabel.position = CGPoint(x: 80, y: 700)
+        addChild(editLabel)
 
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
 
@@ -35,17 +63,41 @@ class GameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let location = touch.location(in: self)
-            let ball = SKSpriteNode(imageNamed: "ballRed")
 
-            ball.name = "ball"
+            let objects = nodes(at: location)
 
-            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+            if objects.contains(editLabel) {
+                editingMode.toggle()
+            } else {
+                if editingMode {
+                    let size = CGSize(width: Int.random(in: 16...128),
+                                      height: 16)
+                    let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1),
+                                                          green: CGFloat.random(in: 0...1),
+                                                          blue: CGFloat.random(in: 0...1),
+                                                          alpha: 1),
+                                           size: size)
+                    box.zRotation = CGFloat.random(in: 0...3)
+                    box.position = location
 
-            ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask
+                    box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
+                    box.physicsBody?.isDynamic = false
 
-            ball.physicsBody?.restitution = 0.4
-            ball.position = location
-            addChild(ball)
+                    addChild(box)
+                } else {
+                    let ball = SKSpriteNode(imageNamed: "ballRed")
+
+                    ball.name = "ball"
+
+                    ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+
+                    ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask
+
+                    ball.physicsBody?.restitution = 0.4
+                    ball.position = location
+                    addChild(ball)
+                }
+            }
         }
     }
     
@@ -73,8 +125,7 @@ class GameScene: SKScene {
             : "slotGlowBad"
         )
 
-        slotBase.name = "good"
-        slotGlow.name = "bad"
+        slotBase.name = isGood ? "good" : "bad"
 
         slotBase.position = position
         slotGlow.position = position
@@ -106,8 +157,10 @@ extension GameScene: SKPhysicsContactDelegate {
     func collisionBetween(ball: SKNode, object: SKNode) {
         if object.name == "good" {
             destroy(ball: ball)
+            score += 1
         } else if object.name == "bad" {
             destroy(ball: ball)
+            score -= 1
         }
     }
 
